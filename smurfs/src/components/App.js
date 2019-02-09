e83beb3fdb74840c5125bf4e14781d4c94ac0a53
jrizza88 <jetorres88@gmail.com>
2019-02-09 11:48:55 -0500

    MVP completed

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -13,10 +13,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <h1>Welcome to the Redux Smurf Village!</h1>
         <Smurfs />
         <SmurfForm />
       </div>
